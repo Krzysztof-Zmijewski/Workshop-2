ef9c4afeb76007372e035ac94cf269a238918b54
Krzysztof-Zmijewski <krzysztof.zmijewski00@wp.pl>
2023-05-18 19:28:07 +0200

    Struktura

---
+++ src/main/java/Workshop/DbUtil.java
@@ -0,0 +1,30 @@
+package Workshop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbUtil {
+    private static final String DB_URL_USERS = "jdbc:mysql://localhost:3306/workshop2";
+
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "coderslab";
+
+    public static Connection connectUSERS() throws SQLException {
+        return DriverManager.getConnection(DB_URL_USERS, DB_USER, DB_PASSWORD);
+    }
+
+
+
+    public static void insert(Connection conn, String query, String... params) {
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            for (int i = 0; i < params.length; i++) {
+                statement.setString(i + 1, params[i]);
+            }
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
